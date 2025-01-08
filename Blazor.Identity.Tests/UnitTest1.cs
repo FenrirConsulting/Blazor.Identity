166732e167e096f3e7bbc438a09856edf33c968e
Christopher Olson <Wolfenstyne@Fenrir.local>
2025-01-08 11:01:07 -0500

    Init

---
+++ Blazor.Identity.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Blazor.Identity.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Blazor.Identity/Program.cs
@@ -0,0 +1,17 @@
+using Blazor.Identity;
+
+#region Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Application Services in Dependency Injection
+#pragma warning disable CA1416
+builder.Services.AddServerUI(builder.Configuration);
+
+var app = builder.Build();
+
+// Build Application Configuration from Dependency Injection
+app.ConfigureServer(builder.Configuration);
+#pragma warning restore CA1416
+
+await app.RunAsync();
+#endregion
